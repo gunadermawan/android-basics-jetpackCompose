c7e7747101402f949fa374ae33d219a1fff0f86d
gunadermawan <gunadermawan.official@gmail.com>
2022-12-16 23:23:36 +0700

    create state onclik button and internal state func with variabel composable

--- app/src/main/java/com/gunder/composebasics/MainActivity.kt
+++ app/src/main/java/com/gunder/composebasics/MainActivity.kt
@@ -8,7 +8,7 @@ import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -44,19 +44,25 @@ private fun MyApp(
 
 @Composable
 fun Greeting(name: String) {
+    val expanded = remember {
+        mutableStateOf(false)
+    }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
             Column(
-                modifier = Modifier.weight(1f)
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
             ) {
                 Text(text = "Hello")
                 Text(text = "$name!")
             }
-            ElevatedButton(onClick = {}) {
-                Text("Show More")
+            ElevatedButton(onClick = { expanded.value = !expanded.value }) {
+                Text(if (expanded.value) "Show Less" else "Show More")
             }
         }
     }
