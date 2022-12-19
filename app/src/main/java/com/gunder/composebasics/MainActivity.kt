9c6f64d6026a10269e7713dd4daf7f4a039224d8
gunadermawan <gunadermawan.official@gmail.com>
2022-12-19 16:38:02 +0700

    config dark mode theme and add dependency icon compose

--- app/src/main/java/com/gunder/composebasics/MainActivity.kt
+++ app/src/main/java/com/gunder/composebasics/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.gunder.composebasics
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -30,6 +31,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Preview(showBackground = true, widthDp = 320, uiMode = UI_MODE_NIGHT_YES, name = "dark moode")
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
