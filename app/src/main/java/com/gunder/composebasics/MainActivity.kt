0ea416e59ce63b630306698c92b0797d39499c1a
gunadermawan <gunadermawan.official@gmail.com>
2022-12-19 17:04:27 +0700

    create card and content inside it, remmomve icon material 3 dependency

--- app/src/main/java/com/gunder/composebasics/MainActivity.kt
+++ app/src/main/java/com/gunder/composebasics/MainActivity.kt
@@ -10,11 +10,20 @@ import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,14 +40,6 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview(showBackground = true, widthDp = 320, uiMode = UI_MODE_NIGHT_YES, name = "dark moode")
-@Preview(showBackground = true, widthDp = 320)
-@Composable
-fun DefaultPreview() {
-    ComposeBasicsTheme {
-        MyApp()
-    }
-}
 
 @Composable
 private fun OnBoardingScreen(onContinueClicked: () -> Unit, modifier: Modifier = Modifier) {
@@ -57,25 +58,35 @@ private fun OnBoardingScreen(onContinueClicked: () -> Unit, modifier: Modifier =
     }
 }
 
-@Preview(showBackground = true, widthDp = 320, heightDp = 320)
-@Composable
-private fun OnBoardingPreview() {
-    ComposeBasicsTheme {
-        OnBoardingScreen(onContinueClicked = {})
-    }
-}
 
 @Composable
 private fun MyApp(modifier: Modifier = Modifier) {
     var shouldShowOnBoarding by rememberSaveable {
         mutableStateOf(true)
     }
-    Surface(modifier) {
+    Surface(modifier, color = MaterialTheme.colorScheme.background) {
         if (shouldShowOnBoarding) {
             OnBoardingScreen(onContinueClicked = { shouldShowOnBoarding = false })
         } else {
             Greetings()
         }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, uiMode = UI_MODE_NIGHT_YES, name = "dark")
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun DefaultPreview() {
+    ComposeBasicsTheme {
+        MyApp()
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+private fun OnBoardingPreview() {
+    ComposeBasicsTheme {
+        OnBoardingScreen(onContinueClicked = {})
     }
 }
 
@@ -107,6 +118,17 @@ private fun GreetingPreview() {
 
 @Composable
 fun Greeting(name: String) {
+    Card(
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primary),
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        CardContent(name)
+
+    }
+}
+
+@Composable
+fun CardContent(name: String) {
     var expanded by rememberSaveable {
         mutableStateOf(false)
     }
@@ -132,6 +154,12 @@ fun Greeting(name: String) {
                     text = "$name!",
                     style = MaterialTheme.typography.headlineMedium.copy(fontWeight = FontWeight.ExtraBold)
                 )
+                if (expanded) {
+                    Text(
+                        text = ("\"Composem ipsum color sit lazy, \" +\n" +
+                                "\"padding theme elit, sed do bouncy. \"").repeat(4)
+                    )
+                }
             }
             ElevatedButton(onClick = { expanded = !expanded }) {
                 Text(if (expanded) "Show Less" else "Show More")
